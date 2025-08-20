1c304e83d5b7a86a9198c87c4ff236bd4398a8bd
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-08-20 10:54:32 +0530

    Athontication backend API creat

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/" , (req , res)=>{
-    res.send("Creat Api , SmartHomeManager")
-})
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
 
-app.listen(5000 ,() => console.log(`Server running on port 5000`))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
